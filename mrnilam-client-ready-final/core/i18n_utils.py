c182b3a5740e218cff656a08144e4f1d52e7f932
Khesha <khesha@test.com>
2026-08-02 14:08:44 +0545

    Fix Nepali translation catalogue and remove nested project copy

--- mrnilam-client-ready-final/core/i18n_utils.py
+++
@@ -1,5 +0,0 @@
-from django.utils.translation import get_language
-
-
-def localized(ne_value, en_value):
-    return ne_value if get_language() == 'ne' else en_value
--- mrnilam-client-ready-final/counseling/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class CounselingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "counseling"
-    verbose_name = "Counseling"
